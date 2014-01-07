c9a756bde7ef2a1ee400f08338f0036c6fcf1992
Ben Langfeld <ben@langfeld.me>
2014-01-07 11:22:11 -0200

    Allow creation and destruction of recognizers, loading and unloading of a single grammar

--- res/res_speech_dtmf.c
+++ res/res_speech_dtmf.c
@@ -24,6 +24,8 @@
 
 #include <asterisk.h>
 
+#include <stdbool.h>
+
 ASTERISK_FILE_VERSION(__FILE__, "$Revision: $")
 
 #include <asterisk/module.h>
@@ -32,6 +34,21 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: $")
 #include <asterisk/speech.h>
 
 #define UNI_ENGINE_NAME "res_speech_dtmf"
+
+/** \brief Forward declaration of speech */
+typedef struct dtmf_recog dtmf_recog;
+
+/** \brief Declaration of DTMF recognizer structure */
+struct dtmf_recog {
+  /* Name of the speech object to be used for logging */
+  const char            *name;
+  /* Asterisk speech base */
+  struct ast_speech     *speech_base;
+  /* Loaded grammar */
+  const char            *grammar;
+  /* Wether or not grammar is active */
+  bool                  active;
+};
 
 static int res_speech_dtmf_create(struct ast_speech *speech, struct ast_format *format);
 static int res_speech_dtmf_load_grammar(struct ast_speech *speech, const char *grammar_name, const char *grammar_path);
@@ -48,37 +65,127 @@ struct ast_speech_result* res_speech_dtmf_get(struct ast_speech *speech);
 /** \brief Set up the speech structure within the engine */
 static int res_speech_dtmf_create(struct ast_speech *speech, struct ast_format *format)
 {
-  return -1;
+  dtmf_recog *recog = malloc(sizeof(dtmf_recog));
+  recog->speech_base = speech;
+  recog->grammar = NULL;
+  recog->active = false;
+  speech->data = recog;
+  return 0;
 }
 
 /** \brief Destroy any data set on the speech structure by the engine */
 static int res_speech_dtmf_destroy(struct ast_speech *speech)
 {
-  return -1;
+  speech->data = NULL;
+  return 0;
 }
 
 /*! \brief Load a local grammar on the speech structure */
 static int res_speech_dtmf_load_grammar(struct ast_speech *speech, const char *grammar_name, const char *grammar_path)
 {
-  return -1;
+  dtmf_recog *recog = speech->data;
+  const char *content_type = NULL;
+  const char *body = NULL;
+  bool inline_content = false;
+  char *tmp;
+
+  if(recog->grammar) {
+    ast_log(LOG_ERROR, "(%s) Unable to load grammar name: %s type: %s path: %s because there is already a grammar\n",
+      recog->name,
+      grammar_name,
+      content_type,
+      grammar_path);
+    return -1;
+  }
+
+  /*
+   * Grammar name and path are mandatory attributes,
+   * grammar type can be optionally specified with path.
+   *
+   * SpeechLoadGrammar(name|path)
+   * SpeechLoadGrammar(name|type:path)
+   * SpeechLoadGrammar(name|uri:path)
+   * SpeechLoadGrammar(name|builtin:grammar/digits)
+  */
+
+  tmp = strchr(grammar_path,':');
+  if(tmp) {
+    const char builtin_token[] = "builtin";
+    const char uri_token[] = "uri";
+    if(strncmp(grammar_path,builtin_token,sizeof(builtin_token)-1) == 0) {
+      content_type = "text/uri-list";
+      inline_content = true;
+      body = grammar_path;
+    }
+    else if(strncmp(grammar_path,uri_token,sizeof(uri_token)-1) == 0) {
+      content_type = "text/uri-list";
+      inline_content = true;
+      grammar_path = tmp+1;
+    }
+    else {
+      *tmp = '\0';
+      content_type = grammar_path;
+      grammar_path = tmp+1;
+    }
+  }
+
+  if(inline_content != true) {
+    ast_log(LOG_WARNING, "(%s) Fetching of grammar %s from file not supported\n",recog->name,grammar_path);
+    return -1;
+  }
+
+  if(!body) {
+    ast_log(LOG_WARNING, "(%s) No grammar content available %s\n",recog->name,grammar_path);
+    return -1;
+  }
+
+  /* Try to implicitly detect content type, if it's not specified */
+  if(!content_type) {
+    if(strstr(body,"#JSGF")) {
+      content_type = "application/x-jsgf";
+    }
+    else if(strstr(body,"#ABNF")) {
+      content_type = "application/srgs";
+    }
+    else {
+      content_type = "application/srgs+xml";
+    }
+  }
+
+  ast_log(LOG_NOTICE, "(%s) Load grammar name: %s type: %s path: %s\n",
+        recog->name,
+        grammar_name,
+        content_type,
+        grammar_path);
+
+  recog->grammar = grammar_path;
+
+  return 0;
 }
 
 /** \brief Unload a local grammar */
 static int res_speech_dtmf_unload_grammar(struct ast_speech *speech, const char *grammar_name)
 {
-  return -1;
+  dtmf_recog *recog = speech->data;
+  recog->grammar = NULL;
+  recog->active = false;
+  return 0;
 }
 
 /** \brief Activate a loaded grammar */
 static int res_speech_dtmf_activate_grammar(struct ast_speech *speech, const char *grammar_name)
 {
-  return -1;
+  dtmf_recog *recog = speech->data;
+  recog->active = true;
+  return 0;
 }
 
 /** \brief Deactivate a loaded grammar */
 static int res_speech_dtmf_deactivate_grammar(struct ast_speech *speech, const char *grammar_name)
 {
-  return -1;
+  dtmf_recog *recog = speech->data;
+  recog->active = false;
+  return 0;
 }
 
 /** \brief Write audio to the speech engine */
